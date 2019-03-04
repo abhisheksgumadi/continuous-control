cb31bedffefcce201f0a964475f34bc7af415f5a
Abhishek Shivkumar <abhishek.shivkumar@onfido.com>
2019-03-04 10:56:06 +0000

    Few minor changes to comments

--- agent.py
+++ agent.py
@@ -14,12 +14,12 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 128  # minibatch size
-GAMMA = 0.99  # discount factor
-TAU = 1e-3  # for soft update of target parameters
-LR_ACTOR = 2e-4  # learning rate of the actor
-LR_CRITIC = 2e-4  # learning rate of the critic
-WEIGHT_DECAY = 0  # L2 weight decay
+BATCH_SIZE = 128        # minibatch size
+GAMMA = 0.99            # discount factor
+TAU = 1e-3              # for soft update of target parameters
+LR_ACTOR = 2e-4         # learning rate of the actor
+LR_CRITIC = 2e-4        # learning rate of the critic
+WEIGHT_DECAY = 0        # L2 weight decay
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
